f284ecf401cd2ade38c80cea4944631283fbb56e
عماره مهدي <89358379+AmaraMeh@users.noreply.github.com>
2025-02-09 11:37:10 +0100

    Update countdown.js 0.1

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,5 +1,5 @@
 function updateCountdown() {
-    const targetDate = new Date("Feb 9, 2025 08:00:00");
+    const targetDate = new Date("Mai 4, 2025 08:00:00");
     const now = new Date();
     const difference = targetDate - now;
 
